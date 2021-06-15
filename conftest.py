c7d348b497f42b3dbdb91d85661131e64ceb1a64
Raqeeb Alam <raqeebalam@quicklly.com>
2021-06-15 23:50:04 +0500

    Merge remote-tracking branch 'github/main' into main

--- conftest.py
+++ conftest.py
@@ -32,29 +32,29 @@ def pytest_runtest_makereport(item, call):
 #     cells.insert(1, html.td(report.description))
 
 
-@pytest.mark.optionalhook
-def pytest_html_results_summary(prefix, summary, postfix):
-    prefix.extend([html.p("Tester: xqc")])
-
-
-def pytest_configure(config):
-    config._metadata['Test Address'] = 'https://10.12.104.18'
-
-
-@pytest.mark.optionalhook
-def pytest_html_results_table_header(cells):
-    cells.insert(2, html.th('Description'))
-    cells.insert(3, html.th('Time', class_='sortable time', col='time'))
-    # cells.insert(1,html.th("Test_nodeid"))
-    cells.pop()
-
-
-@pytest.mark.optionalhook
-def pytest_html_results_table_row(report, cells):
-    cells.insert(2, html.td(report.description))
-    cells.insert(3, html.td(datetime.utcnow(), class_='col-time'))
-    # cells.insert(1,html.td(report.nodeid))
-    cells.pop()
+# @pytest.mark.optionalhook
+# def pytest_html_results_summary(prefix, summary, postfix):
+#     prefix.extend([html.p("Tester: xqc")])
+#
+#
+# def pytest_configure(config):
+#     config._metadata['Test Address'] = 'https://10.12.104.18'
+#
+#
+# @pytest.mark.optionalhook
+# def pytest_html_results_table_header(cells):
+#     cells.insert(2, html.th('Description'))
+#     cells.insert(3, html.th('Time', class_='sortable time', col='time'))
+#     # cells.insert(1,html.th("Test_nodeid"))
+#     cells.pop()
+#
+#
+# @pytest.mark.optionalhook
+# def pytest_html_results_table_row(report, cells):
+#     cells.insert(2, html.td(report.description))
+#     cells.insert(3, html.td(datetime.utcnow(), class_='col-time'))
+#     # cells.insert(1,html.td(report.nodeid))
+#     cells.pop()
 
 #
 # @pytest.mark.hookwrapper
