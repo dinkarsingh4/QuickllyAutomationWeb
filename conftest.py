219b523bd208f2980c830b2a5ab7c4c635f197f2
Raqeeb Alam <raqeebalam@quicklly.com>
2021-06-15 18:55:48 +0500

    Merge remote-tracking branch 'github/main' into main

--- conftest.py
+++ conftest.py
@@ -5,29 +5,29 @@ from py._xmlgen import html
 from datetime import datetime
 
 
-@pytest.mark.optionalhook
-def pytest_html_results_summary(prefix, summary, postfix):
-    prefix.extend([html.p("Tester: xqc")])
-
-
-def pytest_configure(config):
-    config._metadata['Test Address'] = 'https://10.12.104.18'
-
-
-@pytest.mark.optionalhook
-def pytest_html_results_table_header(cells):
-    cells.insert(2, html.th('Description'))
-    cells.insert(3, html.th('Time', class_='sortable time', col='time'))
-    # cells.insert(1,html.th("Test_nodeid"))
-    cells.pop()
-
-
-@pytest.mark.optionalhook
-def pytest_html_results_table_row(report, cells):
-    cells.insert(2, html.td(report.description))
-    cells.insert(3, html.td(datetime.utcnow(), class_='col-time'))
-    # cells.insert(1,html.td(report.nodeid))
-    cells.pop()
+# @pytest.mark.optionalhook
+# def pytest_html_results_summary(prefix, summary, postfix):
+#     prefix.extend([html.p("Tester: xqc")])
+#
+#
+# def pytest_configure(config):
+#     config._metadata['Test Address'] = 'https://10.12.104.18'
+#
+#
+# @pytest.mark.optionalhook
+# def pytest_html_results_table_header(cells):
+#     cells.insert(2, html.th('Description'))
+#     cells.insert(3, html.th('Time', class_='sortable time', col='time'))
+#     # cells.insert(1,html.th("Test_nodeid"))
+#     cells.pop()
+#
+#
+# @pytest.mark.optionalhook
+# def pytest_html_results_table_row(report, cells):
+#     cells.insert(2, html.td(report.description))
+#     cells.insert(3, html.td(datetime.utcnow(), class_='col-time'))
+#     # cells.insert(1,html.td(report.nodeid))
+#     cells.pop()
 
 #
 # @pytest.mark.hookwrapper
@@ -37,6 +37,17 @@ def pytest_html_results_table_row(report, cells):
 #     report.description = str(item.function.__doc__)
 #     report.nodeid = report.nodeid.encode("utf-8").decode("unicode_escape")
 
+
+def pytest_html_results_table_header(cells):
+    cells.insert(2, html.th("Description"))
+    cells.insert(1, html.th("Time", class_="sortable time", col="time"))
+    cells.pop()
+
+
+def pytest_html_results_table_row(report, cells):
+    cells.insert(2, html.td(report.description))
+    cells.insert(1, html.td(datetime.utcnow(), class_="col-time"))
+    cells.pop()
 
 @pytest.mark.hookwrapper
 def pytest_runtest_makereport(item, call):
