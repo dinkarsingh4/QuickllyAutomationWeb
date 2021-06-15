bf68a9effc478b1649dea4fb7903976c05173113
Raqeeb Alam <raqeebalam@quicklly.com>
2021-06-15 23:54:58 +0500

    Merge remote-tracking branch 'github/main' into main

--- conftest.py
+++ conftest.py
@@ -5,21 +5,48 @@ from py._xmlgen import html
 from datetime import datetime
 
 
+import pytest
 @pytest.mark.hookwrapper
-def pytest_runtest_makereport(item, call):
+def pytest_runtest_makereport(item):
+
+    """
+        Extends the PyTest Plugin to take and embed screenshot in html report, whenever test fails.
+        :param item:
+        """
     pytest_html = item.config.pluginmanager.getplugin('html')
     outcome = yield
     report = outcome.get_result()
     extra = getattr(report, 'extra', [])
-    if report.when == 'call':
-        # always add url to report
-        extra.append(pytest_html.extras.url('http://www.dev.quicklly.com/'))
+    file_name = ""
+    if report.when == 'call' or report.when == "setup":
         xfail = hasattr(report, 'wasxfail')
         if (report.skipped and xfail) or (report.failed and not xfail):
-            # only add additional html on failure
-            extra.append(pytest_html.extras.image('/home/excellence/PycharmProjects/gitAutomation/tests/screenshots'))
-            extra.append(pytest_html.extras.html('<div>Additional HTML</div>'))
+            try:
+                file_name=item.obj.__self__.base_page.capture_screen_shot()
+            except Exception as e:
+                print(e)
+            if file_name:
+                html = '<div><img src="%s" alt="screenshot" style="width:304px;height:228px;" ' \
+                       'onclick="window.open(this.src)" align="right"/></div>' % file_name
+                extra.append(pytest_html.extras.html(html))
         report.extra = extra
+
+
+# @pytest.mark.hookwrapper
+# def pytest_runtest_makereport(item, call):
+#     pytest_html = item.config.pluginmanager.getplugin('html')
+#     outcome = yield
+#     report = outcome.get_result()
+#     extra = getattr(report, 'extra', [])
+#     if report.when == 'call':
+#         # always add url to report
+#         extra.append(pytest_html.extras.url('http://www.dev.quicklly.com/'))
+#         xfail = hasattr(report, 'wasxfail')
+#         if (report.skipped and xfail) or (report.failed and not xfail):
+#             # only add additional html on failure
+#             extra.append(pytest_html.extras.image('/home/excellence/PycharmProjects/gitAutomation/tests/screenshots'))
+#             extra.append(pytest_html.extras.html('<div>Additional HTML</div>'))
+#         report.extra = extra
 
 
 # @pytest.mark.optionalhook
