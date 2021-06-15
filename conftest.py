57bda1f1b08ac4656f264d6bef6ffecc5c08d0e6
Raqeeb Alam <raqeebalam@quicklly.com>
2021-06-16 00:42:13 +0500

    Merge remote-tracking branch 'github/main' into main

--- conftest.py
+++ conftest.py
@@ -47,18 +47,18 @@ def pytest_runtest_makereport(item, call):
         report.extra = extra
 
 
-# @pytest.mark.optionalhook
-# def pytest_html_results_table_header(cells):
-#     cells.insert(2, html.th('Description'))
-#     cells.insert(1, html.th('Time', class_='sortable time', col='time'))
-#     cells.pop()
-
-
 @pytest.mark.optionalhook
-def pytest_html_results_table_row(report, cells):
-    cells.insert(2, html.td(report.description))
-    cells.insert(1, html.td(datetime.utcnow(), class_='col-time'))
+def pytest_html_results_table_header(cells):
+    cells.insert(2, html.th('Description'))
+    cells.insert(1, html.th('Time', class_='sortable time', col='time'))
     cells.pop()
+
+
+# @pytest.mark.optionalhook
+# def pytest_html_results_table_row(report, cells):
+#     cells.insert(2, html.td(report.description))
+#     cells.insert(1, html.td(datetime.utcnow(), class_='col-time'))
+#     cells.pop()
 
 
 @pytest.mark.hookwrapper
