4ed8d3d13ca337a671cf3a9c49a8c540708b74b9
Raqeeb Alam <raqeebalam@quicklly.com>
2021-06-15 17:26:14 +0500

    Merge remote-tracking branch 'github/main' into main

--- conftest.py
+++ conftest.py
@@ -38,4 +38,28 @@ def pytest_runtest_makereport(item, call):
     report.nodeid = report.nodeid.encode("utf-8").decode("unicode_escape")
 
 
+@pytest.mark.hookwrapper
+def pytest_runtest_makereport(item, call):
+    timestamp = datetime.now().strftime('%H-%M-%S')
 
+    pytest_html = item.config.pluginmanager.getplugin('html')
+    outcome = yield
+    report = outcome.get_result()
+    extra = getattr(report, 'extra', [])
+    if report.when == 'call':
+
+        feature_request = item.funcargs['request']
+
+        driver = feature_request.getfuncargvalue('browser')
+        driver.save_screenshot('D:/report/scr' + timestamp + '.png')
+
+        extra.append(pytest_html.extras.image('D:/report/scr' + timestamp + '.png'))
+
+        # always add url to report
+        extra.append(pytest_html.extras.url('http://www.example.com/'))
+        xfail = hasattr(report, 'wasxfail')
+        if (report.skipped and xfail) or (report.failed and not xfail):
+            # only add additional html on failure
+            extra.append(pytest_html.extras.image('D:/report/scr.png'))
+            extra.append(pytest_html.extras.html('<div>Additional HTML</div>'))
+        report.extra = extra
