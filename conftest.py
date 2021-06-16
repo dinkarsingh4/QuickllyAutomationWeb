22254cf9f1137c352aa4ad7a36b4bcbd2966d295
Raqeeb Alam <raqeebalam@quicklly.com>
2021-06-16 13:22:37 +0500

    Merge remote-tracking branch 'github/main' into main

--- conftest.py
+++ conftest.py
@@ -78,23 +78,23 @@ def pytest_runtest_makereport(item, call):
             extra.append(pytest_html.extras.html('<div>Additional HTML</div>'))
         report.extra = extra
 
-@pytest.mark.optionalhook
-def pytest_html_results_table_header(cells):
-    cells.insert(2, html.th('Description'))
-    cells.insert(1, html.th('Time', class_='sortable time', col='time'))
-    cells.pop()
-
-
-@pytest.mark.optionalhook
-def pytest_html_results_table_row(report, cells):
-    cells.insert(2, html.td(report.description))
-    cells.insert(1, html.td(datetime.utcnow(), class_='col-time'))
-    cells.pop()
-
-
-@pytest.mark.hookwrapper
-def pytest_runtest_makereport(item, call):
-    outcome = yield
-    report = outcome.get_result()
-    report.description = str(item.function.__doc__)
+# @pytest.mark.optionalhook
+# def pytest_html_results_table_header(cells):
+#     cells.insert(2, html.th('Description'))
+#     cells.insert(1, html.th('Time', class_='sortable time', col='time'))
+#     cells.pop()
+#
+#
+# @pytest.mark.optionalhook
+# def pytest_html_results_table_row(report, cells):
+#     cells.insert(2, html.td(report.description))
+#     cells.insert(1, html.td(datetime.utcnow(), class_='col-time'))
+#     cells.pop()
+#
+#
+# @pytest.mark.hookwrapper
+# def pytest_runtest_makereport(item, call):
+#     outcome = yield
+#     report = outcome.get_result()
+#     report.description = str(item.function.__doc__)
 
