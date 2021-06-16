978441d9c944fe72689b1fa82155f34ce223336b
Raqeeb Alam <raqeebalam@quicklly.com>
2021-06-16 18:54:30 +0500

    Merge remote-tracking branch 'github/main' into main

--- conftest.py
+++ conftest.py
@@ -5,23 +5,23 @@ from datetime import datetime
 import pytest
 
 
-def pytest_html_results_table_header(cells):
-    cells.insert(2, html.th("Description"))
-    cells.insert(1, html.th("Time", class_="sortable time", col="time"))
-    cells.pop()
-
-
-def pytest_html_results_table_row(report, cells):
-    cells.insert(2, html.td(report.description))
-    cells.insert(1, html.td(datetime.utcnow(), class_="col-time"))
-    cells.pop()
-
-
-@pytest.hookimpl(hookwrapper=True)
-def pytest_runtest_makereport(item, call):
-    outcome = yield
-    report = outcome.get_result()
-    report.description = str(item.function.__doc__)
+# def pytest_html_results_table_header(cells):
+#     cells.insert(2, html.th("Description"))
+#     cells.insert(1, html.th("Time", class_="sortable time", col="time"))
+#     cells.pop()
+#
+#
+# def pytest_html_results_table_row(report, cells):
+#     cells.insert(2, html.td(report.description))
+#     cells.insert(1, html.td(datetime.utcnow(), class_="col-time"))
+#     cells.pop()
+#
+#
+# @pytest.hookimpl(hookwrapper=True)
+# def pytest_runtest_makereport(item, call):
+#     outcome = yield
+#     report = outcome.get_result()
+#     report.description = str(item.function.__doc__)
 
 
 @pytest.mark.hookwrapper
@@ -39,6 +39,15 @@ def pytest_runtest_makereport(item, call):
             extra.append(pytest_html.extras.image('screenshots'))
             extra.append(pytest_html.extras.html('<div>djndjnk.fn</div>'))
         report.extra = extra
+    report.description = str(item.function.__doc__)
+
+    @pytest.mark.optionalhook
+    def pytest_html_results_table_header(cells):
+        cells.insert(1, html.th('Description'))
+
+    @pytest.mark.optionalhook
+    def pytest_html_results_table_row(report, cells):
+        cells.insert(1, html.td(report.description))
 
 
 
