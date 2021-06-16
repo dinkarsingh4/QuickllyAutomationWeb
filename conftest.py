06c1f562b9fc2480cd71d6a28a95ce774b8f5132
Raqeeb Alam <raqeebalam@quicklly.com>
2021-06-16 14:21:11 +0500

    Merge remote-tracking branch 'github/main' into main

--- conftest.py
+++ conftest.py
@@ -5,31 +5,6 @@ from datetime import datetime
 import pytest
 
 
-# @pytest.mark.hookwrapper
-# def pytest_runtest_makereport(item):
-#
-#     """
-#         Extends the PyTest Plugin to take and embed screenshot in html report, whenever test fails.
-#         :param item:
-#         """
-#     pytest_html = item.config.pluginmanager.getplugin('html')
-#     outcome = yield
-#     report = outcome.get_result()
-#     extra = getattr(report, 'extra', [])
-#     file_name = "screenshots"
-#     if report.when == 'call' or report.when == "setup":
-#         xfail = hasattr(report, 'wasxfail')
-#         if (report.skipped and xfail) or (report.failed and not xfail):
-#             try:
-#                 file_name=item.obj.__self__.base_page.capture_screen_shot()
-#             except Exception as e:
-#                 print(e)
-#             if file_name:
-#                 html = '<div><img src="%s" alt="screenshot" style="width:304px;height:228px;" ' \
-#                        'onclick="window.open(this.src)" align="right"/></div>' % file_name
-#                 extra.append(pytest_html.extras.html(html))
-#         report.extra = extra
-
 @pytest.mark.hookwrapper
 def pytest_runtest_makereport(item, call):
     pytest_html = item.config.pluginmanager.getplugin('html')
@@ -43,44 +18,25 @@ def pytest_runtest_makereport(item, call):
         if (report.skipped and xfail) or (report.failed and not xfail):
             # only add additional html on failure
             extra.append(pytest_html.extras.image('screenshots'))
-            extra.append(pytest_html.extras.html('<div>Additional HTML</div>'))
+            extra.append(pytest_html.extras.html('<div>djndjnk.fn</div>'))
         report.extra = extra
 
 
-def pytest_html_results_table_header(cells):
-    cells.insert(2, html.th("Description"))
-    cells.insert(1, html.th("Time", class_="sortable time", col="time"))
-    cells.pop()
-
-
-def pytest_html_results_table_row(report, cells):
-    cells.insert(2, html.td(report.description))
-    # cells.insert(1, html.td(datetime.utcnow(), class_="col-time"))
-    cells.pop()
-
-
-@pytest.hookimpl(hookwrapper=True)
-def pytest_runtest_makereport(item, call):
-    outcome = yield
-    report = outcome.get_result()
-    report.description = str(item.function.__doc__)
-
-# @pytest.mark.optionalhook
 # def pytest_html_results_table_header(cells):
-#     cells.insert(2, html.th('Description'))
-#     cells.insert(1, html.th('Time', class_='sortable time', col='time'))
+#     cells.insert(2, html.th("Description"))
+#     cells.insert(1, html.th("Time", class_="sortable time", col="time"))
 #     cells.pop()
 #
 #
-# @pytest.mark.optionalhook
 # def pytest_html_results_table_row(report, cells):
 #     cells.insert(2, html.td(report.description))
-#     cells.insert(1, html.td(datetime.utcnow(), class_='col-time'))
+#     cells.insert(1, html.td(datetime.utcnow(), class_="col-time"))
 #     cells.pop()
 #
 #
-# @pytest.mark.hookwrapper
+# @pytest.hookimpl(hookwrapper=True)
 # def pytest_runtest_makereport(item, call):
 #     outcome = yield
 #     report = outcome.get_result()
 #     report.description = str(item.function.__doc__)
+
