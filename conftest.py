ccdc45ca832a0ec1efd06baee008a71dc458d9b6
Raqeeb Alam <raqeebalam@quicklly.com>
2021-06-17 18:25:40 +0500

    Merge remote-tracking branch 'github/main' into main

--- conftest.py
+++ conftest.py
@@ -18,43 +18,20 @@ def browser():
 
     return driver
 
+
 @pytest.mark.hookwrapper
 def pytest_runtest_makereport(item, call):
-    timestamp = datetime.now().strftime('%H-%M-%S')
-
     pytest_html = item.config.pluginmanager.getplugin('html')
     outcome = yield
     report = outcome.get_result()
     extra = getattr(report, 'extra', [])
     if report.when == 'call':
-
-        feature_request = item.funcargs['request']
-
-        driver = feature_request.getfuncargvalue('browser')
-        driver.save_screenshot('D:/report/scr' + timestamp + '.png')
-
-        extra.append(pytest_html.extras.image('D:/report/scr' + timestamp + '.png'))
-
         # always add url to report
         extra.append(pytest_html.extras.url('http://www.example.com/'))
         xfail = hasattr(report, 'wasxfail')
         if (report.skipped and xfail) or (report.failed and not xfail):
             # only add additional html on failure
-            extra.append(pytest_html.extras.image('D:/report/scr.png'))
+
+            extra.append(pytest_html.extras.image('screenshot.png'))
             extra.append(pytest_html.extras.html('<div>Additional HTML</div>'))
         report.extra = extra
-# @pytest.mark.hookwrapper
-# def pytest_runtest_makereport(item, call):
-#     pytest_html = item.config.pluginmanager.getplugin('html')
-#     outcome = yield
-#     report = outcome.get_result()
-#     extra = getattr(report, 'extra', [])
-#     if report.when == 'call':
-#         # always add url to report
-#         extra.append(pytest_html.extras.url('http://www.example.com/'))
-#         xfail = hasattr(report, 'wasxfail')
-#         if (report.skipped and xfail) or (report.failed and not xfail):
-#             # only add additional html on failure
-#             extra.append(pytest_html.extras.image('D:/report/screenshot.png'))
-#             extra.append(pytest_html.extras.html('<div>Additional HTML</div>'))
-#         report.extra = extra
