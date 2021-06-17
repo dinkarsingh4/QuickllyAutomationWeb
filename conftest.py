42fbc144e94561968875b2a7287f35c935957d37
Raqeeb Alam <raqeebalam@quicklly.com>
2021-06-17 18:19:55 +0500

    Merge remote-tracking branch 'github/main' into main

--- conftest.py
+++ conftest.py
@@ -1,5 +1,7 @@
 # !/usr/bin/python
 # --*-- coding:utf-8 --*--
+from datetime import datetime
+
 import pytest
 import webbrowser
 
@@ -18,21 +20,27 @@ def browser():
 
 @pytest.mark.hookwrapper
 def pytest_runtest_makereport(item, call):
+    timestamp = datetime.now().strftime('%H-%M-%S')
+
     pytest_html = item.config.pluginmanager.getplugin('html')
     outcome = yield
     report = outcome.get_result()
     extra = getattr(report, 'extra', [])
     if report.when == 'call':
 
-        browser.save_screenshot('/home/excellence/PycharmProjects/gitAutomation/screenshot.png')
-        extra.append(pytest_html.extras.image('/home/excellence/PycharmProjects/gitAutomation/screenshot.png'))
+        feature_request = item.funcargs['request']
+
+        driver = feature_request.getfuncargvalue('browser')
+        driver.save_screenshot('D:/report/scr' + timestamp + '.png')
+
+        extra.append(pytest_html.extras.image('D:/report/scr' + timestamp + '.png'))
 
         # always add url to report
         extra.append(pytest_html.extras.url('http://www.example.com/'))
         xfail = hasattr(report, 'wasxfail')
         if (report.skipped and xfail) or (report.failed and not xfail):
             # only add additional html on failure
-            extra.append(pytest_html.extras.image('/home/excellence/PycharmProjects/gitAutomation/screenshot.png'))
+            extra.append(pytest_html.extras.image('D:/report/scr.png'))
             extra.append(pytest_html.extras.html('<div>Additional HTML</div>'))
         report.extra = extra
 # @pytest.mark.hookwrapper
