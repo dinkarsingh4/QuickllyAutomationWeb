d1f365e219c4c4bb3b73484a555b222e3565899a
Raqeeb Alam <raqeebalam@quicklly.com>
2021-07-01 17:37:14 +0500

    Merge remote-tracking branch 'github/main' into main

--- conftest.py
+++ conftest.py
@@ -3,6 +3,58 @@
 from datetime import datetime
 
 import pytest
+
+# @pytest.yield_fixture(scope='session')
+# def browser():
+#     from selenium import webdriver
+#     driver = webdriver.Chrome()
+#     yield driver
+#     driver.quit()
+
+# from selenium import webdriver
+# import pytest
+# driver = None
+#
+#
+# @pytest.mark.hookwrapper
+# def pytest_runtest_makereport(item):
+#     """
+#     Extends the PyTest Plugin to take and embed screenshot in html report, whenever test fails.
+#     :param item:
+#     """
+#     pytest_html = item.config.pluginmanager.getplugin('html')
+#     outcome = yield
+#     report = outcome.get_result()
+#     extra = getattr(report, 'extra', [])
+#
+#     if report.when == 'call' or report.when == "setup":
+#         xfail = hasattr(report, 'wasxfail')
+#         if (report.skipped and xfail) or (report.failed and not xfail):
+#             file_name = report.nodeid.replace("::", "_")+".png"
+#             _capture_screenshot(file_name)
+#             if file_name:
+#                 html = '<div><img src="file:/C:/SeleniumProject/Pytest_HTML_ScreenShot/ScreenShots/%s" alt="screenshot" style="width:600px;height:228px;" ' \
+#                        'onclick="window.open(this.src)" align="right"/></div>'%file_name
+#                 extra.append(pytest_html.extras.html(html))
+#         report.extra = extra
+#
+#
+# def _capture_screenshot(name):
+#     driver.get_screenshot_as_file("C:\\SeleniumProject\\Pytest_HTML_ScreenShot\\ScreenShots\\"+name)
+#     # driver.get_screenshot_as_file(name)
+#
+#
+#
+# @pytest.fixture(scope='session', autouse=True)
+# def browser():
+#     global driver
+#     if driver is None:
+#         driver = webdriver.Chrome()
+#     yield driver
+#     driver.quit()
+#     return driver
+
+import pytest
 import webbrowser
 
 from selenium.webdriver.chrome import options
@@ -11,7 +63,6 @@ from selenium import webdriver
 
 @pytest.fixture(scope='session')
 def browser():
-
     driver = webdriver.Chrome('resources//chromedriver.exe', options=options)
     driver.get('http://www.gogle.com')
     driver.implicitly_wait(5)
@@ -31,7 +82,8 @@ def pytest_runtest_makereport(item, call):
         xfail = hasattr(report, 'wasxfail')
         if (report.skipped and xfail) or (report.failed and not xfail):
             # only add additional html on failure
-
-            extra.append(pytest_html.extras.image('/home/excellence/PycharmProjects/gitAutomation/tests/screenshots/f"screenshot{name}.png"'))
+            name = datetime.strftime(datetime.now(), '%m-%d_%H-%M-%S')
+            extra.append(pytest_html.extras.image(
+                "/home/excellence/PycharmProjects/gitAutomation/tests/screenshots/"+f'screenshot{name}.png'))
             extra.append(pytest_html.extras.html('<div>Additional HTML</div>'))
         report.extra = extra
