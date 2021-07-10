bc20da0e10714ef99abc0c96004a4eaa245819a2
Raqeeb Alam <raqeebalam@quicklly.com>
2021-07-10 16:09:18 +0500

    Merge remote-tracking branch 'github/main' into main

--- conftest.py
+++ conftest.py
@@ -79,29 +79,57 @@ from selenium import webdriver
 #         report.extra = extra
 
 
-@pytest.fixture(scope='session')
-def browser():
-    driver = webdriver.Chrome('resources//chromedriver.exe', options=options)
-    driver.get('http://www.dev.quicklly.com')
-    driver.implicitly_wait(5)
+# @pytest.fixture(scope='session')
+# def browser():
+#     driver = webdriver.Chrome('resources//chromedriver.exe', options=options)
+#     driver.get('http://www.dev.quicklly.com')
+#     driver.implicitly_wait(5)
+#
+#     return driver
 
-    return driver
+
+# @pytest.mark.hookwrapper
+# def pytest_runtest_makereport(item, call):
+#     pytest_html = item.config.pluginmanager.getplugin('html')
+#     outcome = yield
+#     report = outcome.get_result()
+#     extra = getattr(report, 'extra', [])
+#     if report.when == 'call':
+#         # always add url to report
+#         extra.append(pytest_html.extras.url('http://www.dev.quicklly.com/'))
+#         xfail = hasattr(report, 'wasxfail')
+#         if (report.skipped and xfail) or (report.failed and not xfail):
+#             # only add additional html on failure
+#             name = datetime.strftime(datetime.now(), '%m-%d_%H-%M-%S')
+#             extra.append(pytest_html.extras.image(
+#                 "/var/lib/jenkins/workspace/Quicklly/screenshots"+f'screenshot{name}.png'))
+#             extra.append(pytest_html.extras.html('<div>Additional HTML</div>'))
+#         report.extra = extra
+
+import pytest
 
 
 @pytest.mark.hookwrapper
-def pytest_runtest_makereport(item, call):
+def pytest_runtest_makereport(item):
+
+    """
+        Extends the PyTest Plugin to take and embed screenshot in html report, whenever test fails.
+        :param item:
+        """
     pytest_html = item.config.pluginmanager.getplugin('html')
     outcome = yield
     report = outcome.get_result()
     extra = getattr(report, 'extra', [])
-    if report.when == 'call':
-        # always add url to report
-        extra.append(pytest_html.extras.url('http://www.dev.quicklly.com/'))
+    file_name = ""
+    if report.when == 'call' or report.when == "setup":
         xfail = hasattr(report, 'wasxfail')
         if (report.skipped and xfail) or (report.failed and not xfail):
-            # only add additional html on failure
-            name = datetime.strftime(datetime.now(), '%m-%d_%H-%M-%S')
-            extra.append(pytest_html.extras.image(
-                "/var/lib/jenkins/workspace/Quicklly/screenshots"+f'screenshot{name}.png'))
-            extra.append(pytest_html.extras.html('<div>Additional HTML</div>'))
+            try:
+                file_name = item.obj.__self__.base_page.capture_screen_shot()
+            except Exception as e:
+                print(e)
+            if file_name:
+                html = '<div><img src="%s" alt="screenshot" style="width:304px;height:228px;" ' \
+                       'onclick="window.open(this.src)" align="right"/></div>' % file_name
+                extra.append(pytest_html.extras.html(html))
         report.extra = extra
