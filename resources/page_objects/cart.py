e0af9a21bd90b68f04174b2d57d68b1d87dba2a3
Raqeeb Alam <raqeebalam@quicklly.com>
2021-11-05 21:14:03 +0500

    Merge remote-tracking branch 'github/main' into main

--- resources/page_objects/cart.py
+++ resources/page_objects/cart.py
@@ -207,9 +207,9 @@ class Cart(BasePage):
         button.click()
 
     def select_dropdown(self):
-        element = self.driver.find_element_by_xpath('//*[@id="searchhide"]/header/div[3]')
-        self.driver.execute_script("arguments[0].click();", element)
-        # self.click(MiniCart.yourAccount)
+        # element = self.driver.find_element_by_xpath('//*[@id="searchhide"]/header/div[3]')
+        # self.driver.execute_script("arguments[0].click();", element)
+        self.click(MiniCart.yourAccount)
 
     def click_changeMethod(self):
         time.sleep(15)
