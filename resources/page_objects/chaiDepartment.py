80ca087e154bc78dea566d1d1527ab046a248426
Raqeeb Alam <raqeebalam@quicklly.com>
2021-12-01 21:27:39 +0500

    Merge remote-tracking branch 'github/main' into main

--- resources/page_objects/chaiDepartment.py
+++ resources/page_objects/chaiDepartment.py
@@ -17,7 +17,7 @@ class CACD(BasePage):
         self.click(ChaiAndCoffee.submit_zip)
 
     def click_ChaiAndCoffee(self):
-        element = self.driver.find_element_by_css_selector('#searchhide > div.grocerySpecialSlider.clsFoodSpl > div > div > div > div > a.slick-slide.slick-current.slick-active > img')
+        element = self.driver.find_element_by_xpath('//*[@id="searchhide"]/div[6]/div/div/div/div/a[4]/img')
         self.driver.execute_script("arguments[0].click();", element)
 
     def click_weekly(self):
